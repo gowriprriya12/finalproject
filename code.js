5427e9abc1c42b8c3bcb18a2835800500378524f
gowriprriya12 <144832266+gowriprriya12@users.noreply.github.com>
2023-10-02 10:48:34 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,259 @@
+
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "Inside which HTML element do we put the JavaScript?",
+    "options": [
+    {
+    "a": "&lt;script&gt;",
+    "b": "&lt;javascript&gt;",
+    "c": "&lt;scripting&gt;",
+    "d": "&lt;js&gt;"
+    }
+    ],
+    "answer": "&lt;script&gt;",
+    "score": 0,
+    "status": ""
+    },
+      
+    {
+    "id": 2,
+    "question": "The external JavaScript file must contain the &lt;script&gt; tag.",
+    "options": [
+    {
+    "a": "True",
+    "b": "False"
+    }
+    ],
+    "answer": "False",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "How do you write &quot;Hello World&quot; in an alert box?",
+    "options": [
+    {
+    "a": "alertBox(&quot;Hello World&quot;);",
+    "b": "msg(&quot;Hello World&quot;);",
+    "c": "alert(&quot;Hello World&quot;);",
+    "d": "msgBox(&quot;Hello World&quot;);",
+    }
+    ],
+    "answer": "alert(&quot;Hello World&quot;);",
+    "score": 0,
+    "status": ""
+    },
+
+    {
+    "id": 4,
+    "question": "How to write an IF statement in JavaScript?",
+    "options": [
+    {
+    "a": "if i = 5 then",
+    "b": "if i == 5 then",
+    "c": "if (i == 5)",
+    "d": " if i = 5",
+    }
+    ],
+    "answer": "if (i == 5)",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "Which of the following is a disadvantage of using JavaScript?",
+    "options": [
+    {
+    "a": "Client-side JavaScript does not allow the reading or writing of files.",
+    "b": "JavaScript can not be used for Networking applications because there is no such support available.",
+    "c": "JavaScript doesn't have any multithreading or multiprocess capabilities.",
+    "d": "All of the above."
+    }
+    ],
+    "answer": "All of the above.",
+    "score": 0,
+    "status": ""
+    },
+   
+    {
+    "id": 6,
+    "question": "How does a WHILE loop start?",
+    "options": [
+    {
+    "a": "while i = 1 to 10",
+    "b": "while (i &lt;= 10; i++)",
+    "c": "while (i &lt;= 10)"
+    }
+    ],
+    "answer": "while (i &lt;= 10)",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 7,
+    "question": "How does a FOR loop start?",
+    "options": [
+    {
+    "a": "for (i = 0; i &lt;= 5)",
+    "b": "for (i = 0; i &lt;= 5; i++)",
+    "c": "for i = 1 to 5",
+    "d": "for (i &lt;= 5; i++)"
+    }
+    ],
+    "answer": "for (i = 0; i &lt;= 5; i++)",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "How can you add a comment in a JavaScript?",
+    "options": [
+    {
+    "a": "//This is a comment",
+    "b": "&sbquo;This is a comment",
+    "c": "&lt;!--This is a comment--&gt;"
+    }
+    ],
+    "answer": "//This is a comment",
+    "score": 0,
+    "status": ""
+    },
+    
+    
+    
+    
+    {
+    "id": 9,
+    "question": "JavaScript is the same as Java.",
+    "options": [
+    {
+    "a": "true",
+    "b": "false"
+    }
+    ],
+    "answer": "false",
+    "score": 0,
+    "status": ""
+    },
+    
+
+    {
+    "id": 10,
+    "question": "Which operator is used to assign a value to a variable?",
+    "options": [
+    {
+    "a": "*",
+    "b": "-",
+    "c": "=",
+    "d": "x"
+    }
+    ],
+    "answer": "=",
+    "score": 0,
+    "status": ""
+    },
+    
+    
+    
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
